9f67557b856b4a66b4ec04e7a07f1e0b41ccc212
Marco Lamina <marco.lamina@sap.com>
2023-01-20 09:35:21 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {useState} from "react"
 
 function App() {
+
+    let playing = false;
+    const [buttonText, setButtonText] = useState("Basic Form");
+    const synth = window.speechSynthesis;
+
+    const speak = (text) => {
+        const msg = new SpeechSynthesisUtterance()
+        msg.text = text
+        msg.rate = 0.9
+        synth.speak(msg);
+    };
+
+
+    const basicForm = ["Shift the balance to the right foot, pivot ninety degrees to the left, assume a front stance and down block.",
+    "Step forward to a front stance, single middle punch.",
+    "Shift the balance to the left foot, pivot one-hundred eighty degrees to the right, assume a front stance and down block."]
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const speakForm = async (form) => {
+        speak("Basic Form. Ready, begin.")
+        for (let step in form) {
+            if (!playing) break;
+            speak(form[step])
+            delay(1000)
+        }
+    }
+    const handleClick = () => {
+        if (playing) {
+            console.log("Stopping")
+            setButtonText("Basic Form")
+            playing = false
+            synth.cancel()
+        } else {
+            console.log("Starting")
+            setButtonText("STOP")
+            playing = true
+            speakForm(basicForm)
+        }
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'>
+        <h1>Forms Teacher</h1>
+          <div>
+              <button type="button" onClick={handleClick}>{buttonText}</button>
+          </div>
+      </div>
+  )
 }
 
 export default App;
